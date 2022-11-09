1d8423cdc8ea94f65d81d75117901e10030dd768
dev <73976929+laurikat19@users.noreply.github.com>
2022-11-09 16:16:57 -0500

    representation clase

--- backend/clase/api/serializers/serializer.py
+++ backend/clase/api/serializers/serializer.py
@@ -6,3 +6,14 @@ class ClaseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Clase
         fields = '__all__'
+
+    def to_representation(self, instance):
+        return {
+            "id": instance.id,
+            "salon": instance.salon,
+            "horario": instance.Horario,
+            "profesor_id": instance.profesor.id,
+            "profesor": instance.profesor.nombre,
+            "asignatura_id": instance.asignatura.id,
+            "asignatura": instance.asignatura.nombre,
+        }
